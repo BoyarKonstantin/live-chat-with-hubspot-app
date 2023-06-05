9c2bf8847d33629f63e901916f56d94179fb7a34
Kostiantyn Boiar <97537403+BoyarKonstantin@users.noreply.github.com>
2023-06-05 16:34:16 +0100

    Update CURLCheck.ps1

--- CURLCheck.ps1
+++ CURLCheck.ps1
@@ -1,15 +1,21 @@
-token="dal:3kfmrh8Drk814qWH-AqA3RCwxS0"
-account_id="d72aabe6-77b9-439d-b789-4cb04ccd6593"
-customer_id="78fb8a49-bdfb-42a2-9ca2-36201162d06e"
-agent_id="d72aabe6-77b9-439d-b789-4cb04ccd6593"
+$token = "YOUR_TOKEN"
+$url = "https://api.livechatinc.com/v3.5/agent/action/start_chat"
 
-curl -X POST 
-  -H "Authorization: Bearer $token" 
-  -H "Content-Type: application/json" 
-  -u "$account_id:$token" 
-  -d '{
-    "user_id": "'"$customer_id"'",
-    "user_type": "customer",
-    "agent_id": "'"$agent_id"'"
-  }' 
-  "https://api.livechatinc.com/v3.5/agent/action/start_chat"
+$headers = @{
+    "Authorization" = "Bearer $token"
+    "Content-Type"  = "application/json"
+}
+
+$body = @{
+    "user_id"   = "78fb8a49-bdfb-42a2-9ca2-36201162d06e"
+    "user_type" = "customer"
+    "agent_id"  = "d72aabe6-77b9-439d-b789-4cb04ccd6593"
+} | ConvertTo-Json
+
+$response = Invoke-RestMethod -Method Post -Uri $url -Headers $headers -Body $body
+
+if ($response.StatusCode -eq 200) {
+    Write-Output "Chat created successfully."
+} else {
+    Write-Output "Failed to create chat: $($response | ConvertTo-Json -Depth 100)"
+}
